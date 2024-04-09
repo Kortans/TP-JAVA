44d52652f8dcd4349393865352a133b032a077d9
Kortans <kortansyolo@gmail.com>
2024-04-09 09:28:24 +0200

    Création et appel de la fonction run(80) de la classe WebServer

--- WebServerApplication.java
+++ WebServerApplication.java
@@ -1,6 +1,19 @@
+import java.io.IOException;
+
 public class WebServerApplication {
 
-public static void main (String[] args){
+    WebServerApplication(){}
 
+public static void main (String[] args)
+{
+    
+    WebServer webserver = new WebServer(80);
+    try{
+        webserver.run(80);
+    }
+    catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+    }
 }
 }
