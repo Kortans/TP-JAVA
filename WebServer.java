afaf74c795f939f7533b8ac9fa12ecbe219e44f5
Kortans <kortansyolo@gmail.com>
2024-04-09 09:26:29 +0200

    Mise en place du constructeur, des méthodes readRequest, sendResponse et run

--- WebServer.java
+++ WebServer.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,6 +9,13 @@ import java.net.Socket;
 public class WebServer extends WebServerApplication {
 
     
+    private int portNumber;
+
+    WebServer(){}
+
+    WebServer(int newPortNumber){
+        portNumber = newPortNumber;
+    }
     
     
     private void readRequest(Socket socket) throws IOException{
@@ -15,11 +23,32 @@ public class WebServer extends WebServerApplication {
         InputStreamReader reader = new InputStreamReader(input); 
    }
     private void sendResponse(Socket socket){
-        
+        try {
+            OutputStream output = socket.getOutputStream();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
     public void run(int portNumber) throws IOException{
-        ServerSocket serverSocket = new ServerSocket(portNumber);
-        readRequest(serverSocket);
+        try{
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            System.out.println("Server started on port "+portNumber);    
+       
+        while (true){
+            Socket clienSocket = serverSocket.accept();
+
+            readRequest(clienSocket);
+            sendResponse(clienSocket);
+
+            clienSocket.close();
+        }
+        } 
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    
     }
 }
